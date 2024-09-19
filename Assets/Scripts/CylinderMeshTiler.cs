d7e1c55e61cfe5d4061c8a0c3f30f9b4f3ae9891
Aleko Samblanet <aleko.samblanet.2021@gmail.com>
2024-09-19 11:28:33 -0400

    Tree Bark
    I found a fairly high-res texture and made a material from that. I also downloaded a tree bark pack for reference, but I don't think they're high enough res

---
+++ Assets/Scripts/CylinderMeshTiler.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderMeshTiler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float xScale = gameObject.transform.localScale.x;
+        float yScale = gameObject.transform.localScale.y;
+
+
+    }
+
+}
