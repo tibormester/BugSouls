9ebb35c05dff20b4f06e1de7e3251ccbd964efb5
Aleko Samblanet <aleko.samblanet.2021@gmail.com>
2024-10-03 15:06:39 -0400

    AI Chaser Update
    Ensured AI facing player if within maxEngageDistance, and added unstuck feature and max chase distance to AI, among other changes

--- Assets/Scripts/AIChaser.cs
+++ Assets/Scripts/AIChaser.cs
@@ -7,29 +7,91 @@ public class NavMeshAgentChaser : MonoBehaviour
 {
     
     [SerializeField]
-    private Transform target;   
-    private CharacterMovement charMovement; //The target's movement script to call movement functions on
+    [Tooltip("The target transform the AI is chasing")]
+    private Transform target;
+
     [SerializeField]
-    private float minDistance = 2f, maxDistance = 3f;              //Min distance to move towards player
+    [Tooltip("The minimum and maximum distance the AI wants to stay near the target")]
+    private float minEngageDistance = 2f, maxEngageDistance = 3f;
+
+    [SerializeField]
+    [Tooltip("The speed multiplier the AI uses to back up from the target when too close")]
+    private float backupSpeedMultiplier = 4f;
+
+    [SerializeField]
+    [Tooltip("The maximum distance the AI has to be to the target in order to chase it")]
+    private float maxChaseDistance = 30f;
+
+    [SerializeField]
+    [Tooltip("If stuck while chasing target, this is the amount of time in seconds after which the AI will jump")]
+    private float maxStuckTime = 4f;
+
+    [SerializeField]
+    [Tooltip("This is the distance the AI has to move while chasing the player to be considered not stuck")]
+    private float maxStuckDistance = 1f;
+
+
+    private float stuckTime = 0f;                       //Elapsed time "stuck"
+    private Vector3 stuckPos;                           //Position to test if stuck
+    private bool chasing = false;                       //True if within range and thus chasing player
+    private CharacterMovement charMovement;             //The AI's movement script to call movement functions on
+    private CustomPhysicsBody physicsBody;              //The AI's physics script to find gravity
 
 
     void Start()
     {
         charMovement = GetComponent<CharacterMovement>();
+        physicsBody = GetComponent<CustomPhysicsBody>();
+        stuckPos = transform.position;
     }
 
     void Update()
     {
         if (target != null)
         {
-            if (Vector3.Distance(target.position, transform.position) > maxDistance)
+            if (Vector3.Distance(target.position, transform.position) < maxChaseDistance)
             {
-                charMovement.Move((target.position - transform.position).normalized);
+                chasing = true;
+
+                
+
+                if (Vector3.Distance(target.position, transform.position) > maxEngageDistance)
+                {
+                    charMovement.Move((target.position - transform.position).normalized);
+                    
+                    if (Vector3.Distance(stuckPos, transform.position) < maxStuckDistance)
+                    {
+                        stuckTime += Time.deltaTime;
+
+                        if (stuckTime > maxStuckTime)
+                        {
+                            charMovement.Jump();
+                        }
+                    }
+                    else
+                    {
+                        stuckPos = transform.position;
+                        stuckTime = 0f;
+                    }
+                }
+                else
+                {
+                    stuckTime = 0f;
+
+                    transform.LookAt(target, physicsBody.groundNormal);
+
+                    if (Vector3.Distance(target.position, transform.position) < minEngageDistance)
+                    {
+                        charMovement.Move((transform.position - target.position).normalized, backupSpeedMultiplier);
+                    }
+                }
             }
-            else if (Vector3.Distance(target.position, transform.position) < minDistance)
+            else
             {
-                charMovement.Move((transform.position - target.position).normalized);
+                chasing = false;
             }
+
+            
         }
     }
 
