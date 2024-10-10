b62f118f6541c6c2f3f7b293c5332535df24bb97
Aleko Samblanet <aleko.samblanet.2021@gmail.com>
2024-10-10 12:49:41 -0400

    Anim walk backwards

--- Assets/Scripts/CharacterAnimation.cs
+++ Assets/Scripts/CharacterAnimation.cs
@@ -1,43 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.Animations;
+using UnityEditorInternal;
+using TMPro;
+
 
 public class CharacterAnimation : MonoBehaviour
 {
     private CharacterMovement charMove;
     private CustomPhysicsBody cusPhysBod;
     private Animator charAnimator;
-    private bool walking;
-    private bool running;
-    private bool jumping;
+
+    private float currSpeed;
+    private bool backwards;
 
 
     [SerializeField]
     private float minWalkingSpeed = 1f;
-    [SerializeField]
     private float minRunningSpeed = 2f;
 
 
-    private float currSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-
         charMove = GetComponent<CharacterMovement>();
         cusPhysBod = GetComponent<CustomPhysicsBody>();
         charAnimator = GetComponentInChildren<Animator>();
 
-        minRunningSpeed = charMove.moveSpeed;
+        minRunningSpeed = charMove.moveSpeed * 1.1f;
+        backwards = false;
+        currSpeed = 0f;
     }
+
 
     // Update is called once per frame
     void Update()
     {
         string debug = "null";
 
-        float currSpeed = charMove.GetHorizontalVelocity().magnitude;
+        currSpeed = charMove.GetHorizontalVelocity().magnitude;
+        
+        backwards = Vector3.Angle(charMove.GetHorizontalVelocity(), charMove.look_direction) > 90f;
+
 
         if (charAnimator != null)
         {
@@ -45,23 +51,31 @@ public class CharacterAnimation : MonoBehaviour
             {
                 charAnimator.SetBool("isRunning", true);
                 //charAnimator.SetBool("isWalking", false);
+
+
+
                 debug = "running";
             }
             else if (currSpeed > minWalkingSpeed)
             {
                 charAnimator.SetBool("isWalking", true);
-                //charAnimator.SetBool("isRunning", false);
+                charAnimator.SetBool("isRunning", false);
+
+                charAnimator.SetFloat("speed", (currSpeed/minRunningSpeed)*(backwards ? -1.5f : 1.5f));
+
                 debug = "walking";
             }
             else
             {
                 charAnimator.SetBool("isRunning", false);
                 charAnimator.SetBool("isWalking", false);
+
                 debug = "idle";
             }
+            
         }
 
-        Debug.Log(debug);
+        Debug.Log("Min Running Speed: " + minRunningSpeed + " and "+ debug + " at speed of " + currSpeed);
         
     }
 }
