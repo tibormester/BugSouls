fa8519f0207becfb57eb4a2076a24db4bd894a18
Tibor <tiborcmester@gmail.com>
2024-11-24 08:45:11 -0500

    removed animation controller bs

--- Assets/Scripts/Mechanics/CharacterAnimation.cs
+++ Assets/Scripts/Mechanics/CharacterAnimation.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEditor.Animations;
 using UnityEditorInternal;
 using TMPro;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 
 public class CharacterAnimation : MonoBehaviour
@@ -12,15 +14,11 @@ public class CharacterAnimation : MonoBehaviour
     private CustomPhysicsBody cusPhysBod;
     private Animator charAnimator;
 
-    private float currSpeed;
-    private bool backwards;
-    private bool strafeLeft;
-    private bool strafeRight;
 
 
     [SerializeField]
-    private float minWalkingSpeed = 1f;
-    private float minRunningSpeed = 2f;
+    private float minWalkingSpeed2 = 1f;
+    private float minRunningSpeed2 = 2f;
 
 
 
@@ -31,113 +29,96 @@ public class CharacterAnimation : MonoBehaviour
         cusPhysBod = GetComponent<CustomPhysicsBody>();
         charAnimator = GetComponentInChildren<Animator>();
 
-        minRunningSpeed = charMove.moveSpeed * 1.1f;
-        backwards = false;
-        currSpeed = 0f;
+        minRunningSpeed2 = (charMove.moveSpeed * 1.1f) * (charMove.moveSpeed * 1.1f);
     }
 
 
-    // Update is called once per frame
-    private int isJumping = -1;
-
-    private int combo = 0;
-    private int cooldown = -1;
-    private int expiration = -1;
+    // Update is called once per fram
+    private int combo = 1;//1,2,3
+    private int cooldown = -1;//goes down by one per frame until it is -1
+    private int expiration = -1;//goes down by one per frame until it is -1
     void Update()
     {    
         //TODO implement an input buffer
         //If the user clicks, if we arent in cooldown, attack
         //If we are expired, reset the combo, otherwise incrmenet the combo
-        if(Input.GetKeyDown(KeyCode.Mouse0)){
-            
+        if(Input.GetKeyDown(KeyCode.Mouse0)){    
             if(cooldown < 0){
-                if(expiration > 50 || combo > 2){
-                    combo = 0;
+                //Reset the combo if it expired or u reach the max
+                if(expiration < 0 || combo > 3){
+                    combo = 1;
                 }
-                charAnimator.SetBool("attacking", true);
-                charAnimator.SetInteger("combo", combo);
-                Debug.LogWarning("We are ATTACKING!");
+                ChangeAnimation("1handed combo " + combo);
                 combo += 1;
 
-                expiration = 0;
-                cooldown = 15;
-            } else {
-                charAnimator.SetBool("attacking", false);
-                expiration++;
-                cooldown--;
-            }
-        } else{
-            charAnimator.SetBool("attacking", false);
-            expiration++;
-            cooldown--;
-        }
-
-
-        //COuld be optimized by cachine some values and only doing the vector operaitons a single time instead of for each direction
-
-        string debug = "null";
-        currSpeed = charMove.GetHorizontalVelocity().magnitude;
-        float angle = Vector3.Angle(charMove.GetHorizontalVelocity(), charMove.look_direction);
-
-        //Detect if we are moving up and try to jump if we havent already done so in the past few frames
-        var verticalVelocity = Vector3.Dot(GetComponent<Rigidbody>().velocity, charMove.transform.up);
-        if (verticalVelocity > 1f && isJumping == -1){
-            charAnimator.SetBool("isJumping", true);
-            isJumping = 0;
-        } else{
-            charAnimator.SetBool("isJumping", false);
-            if(isJumping > 0){
-                isJumping++;
-            }
-            if(isJumping > 80){
-                isJumping = -1;
-            }
-        }
-        backwards = angle > 90f + 30f;
-
-        Debug.Log("angle to right: " + Vector3.Angle(charMove.GetHorizontalVelocity(), transform.right));
-        strafeRight = Vector3.Angle(charMove.GetHorizontalVelocity(), transform.right) < 30f;
-        strafeLeft = Vector3.Angle(charMove.GetHorizontalVelocity(), -transform.right) < 30f;
-        if(backwards){
-            charAnimator.SetInteger("walkDirection", 2);
-        }
-        else if (strafeRight){
-            charAnimator.SetInteger("walkDirection", 1);
-        }
-        else if (strafeLeft){
-            charAnimator.SetInteger("walkDirection", -1);
-        }else{
-            charAnimator.SetInteger("walkDirection", 0);
-        }
-
-        Debug.Log("Strafing: left: " + strafeLeft + ", Right: " + strafeRight);
-        if (charAnimator != null)
-        {
-            if (currSpeed > minRunningSpeed){
-                charAnimator.SetBool("isRunning", true);
-                charAnimator.SetBool("isWalking", false);
-
-                debug = "running";
-            }else if (currSpeed > minWalkingSpeed){
-                charAnimator.SetBool("isWalking", true);
-                charAnimator.SetBool("isRunning", false);
-
-                debug = "walking";
-            }else{
-                charAnimator.SetBool("isRunning", false);
-                charAnimator.SetBool("isWalking", false);
-
-                debug = "idle";
-            }
-            
-        }
-        if (!cusPhysBod.IsGrounded()){
-            charAnimator.SetBool("isGrounded", false);
-        } else{
-            charAnimator.SetBool("isGrounded", true);
-        }
-        Debug.Log("direction: " + charAnimator.GetInteger("walkDirection") + " and "+ debug + " at speed of " + currSpeed);
+                //Set the expiration and cooldown timers, maybe add code so this is unique per attack
+                expiration = 500;
+                cooldown = 200;
+                return;
+            } 
+        } 
+        expiration = expiration < 0 ? -1 : expiration - 1;
+        cooldown = cooldown < 0 ? -1 : cooldown - 1;
         
+
+        var horizontalVelocity = charMove.GetHorizontalVelocity();
+        var verticalVelocity = Vector3.Dot(GetComponent<Rigidbody>().velocity, charMove.transform.up);
+
+        float angle = Vector3.SignedAngle(horizontalVelocity, transform.forward, transform.up);
+        
+        bool grounded = cusPhysBod.IsGrounded();
+        bool jumping = verticalVelocity > 3f;
+
+        Debug.Log("angle from straight: " + angle);
+        
+        var speed2 = horizontalVelocity.sqrMagnitude;
+        //Are we on the ground?
+        if(grounded){
+            //Are we trying to jump?
+            if(jumping){
+                ChangeAnimation("Jump");
+            //Are we fast enough to run?
+            } else if (speed2 > minRunningSpeed2) {
+                ChangeAnimation("Run");
+            //Are we fast enough to walk
+            } else if (speed2 > minWalkingSpeed2){
+                //Which direction are we strafing?
+                if (angle < -120 || angle > 120){
+                ChangeAnimation("Back Strafe");
+                } else if ( angle < -30) {
+                    ChangeAnimation("Right Strafe");
+                } else if (angle > 30){
+                    ChangeAnimation("Left Strafe");
+                } else {
+                    ChangeAnimation("Walk Forward");
+                }
+            } else {
+                ChangeAnimation("Idle");
+            }
+        } else {
+            ChangeAnimation("Falling");
+        }
+        
+    }
+
+    //Swaps into the new animation only if its not already playing and if its not one of the unCancellable
+    public string currentAnimation;
+    public static string[] nonCancellable = new string[] {"1handed combo 1", "1handed combo 2","1handed combo 3"};
+
+    public void ChangeAnimation(string animationName){
+        if( currentAnimation == animationName){
+            return;
+        }
+        //If its a unCancellable check to see if its almost done before aborting
+        if (nonCancellable.Contains(currentAnimation)){
+            var state = charAnimator.GetCurrentAnimatorStateInfo(0);
+            if (state.normalizedTime < 0.9){
+                return;
+            }
+        }
+
+        charAnimator.Play(animationName);
+        currentAnimation = animationName;
     }
     
 }
