205b7f556f0e64ce23a6e7ab8153a426216e0873
SZX73 <shafeen7330@gmail.com>
2024-11-27 02:57:06 -0500

    Added enemy healthbars and stamina bar to the hud

---
+++ Assets/Scripts/Mechanics/EnemyHealth.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float maxHealth = 100f;
+    public float currentHealth;
+    [SerializeField] FloatingHealthBar healthBar;
+
+    private ParticleSystem particleSystem;
+
+    private void Start()
+    {
+        currentHealth = maxHealth; // Initialize health
+        particleSystem = GetComponent<ParticleSystem>();
+        healthBar = GetComponentInChildren<FloatingHealthBar>();
+    }
+
+    public void ApplyDamage(float damageAmount)
+    {
+        currentHealth -= damageAmount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth); // Ensure health doesn't go below 0
+        healthBar.UpdateHealthBar(currentHealth, maxHealth);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+        StartCoroutine(PlayHitParticles());
+        
+    }
+    private IEnumerator PlayHitParticles(){
+        particleSystem?.Play();
+        yield return new WaitForSeconds(0.3f);
+        particleSystem?.Stop();
+    }
+
+    private void Die()
+    {
+        // Handle the object's death (e.g., disable it, play animation, etc.)
+        Debug.Log($"{gameObject.name} has died!");
+        gameObject.SetActive(false); // Example action on death
+    }
+}
--- Assets/Scripts/Mechanics/Stamina.cs
+++ Assets/Scripts/Mechanics/Stamina.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
 
 public class Stamina : MonoBehaviour
 {
     public CustomPhysicsBody physicsBody;
     public StaminaBar stamina;
+    public Animator player;
     public float maxStamina = 50f;
     public float currStamina;
     // Start is called before the first frame update
     void Start()
     {
         physicsBody = GetComponent<CustomPhysicsBody>();
+        player = GetComponent<Animator>();
         stamina.setMaxStamina(maxStamina);
         currStamina = maxStamina;
         stamina.setStamina(maxStamina);
@@ -23,7 +26,12 @@ public class Stamina : MonoBehaviour
         if(Input.GetKey(KeyCode.LeftShift) && (Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("d") || Input.GetKey("s")))
         {
             currStamina -= 5 * Time.deltaTime;
-            if(currStamina < 0) currStamina = 0;
+            if(currStamina <= 0) 
+            {
+                currStamina = 0;
+                player.SetBool("isWalking", true);
+                
+            }
             stamina.setStamina(currStamina);
             if(Input.GetKeyDown(KeyCode.Space) && physicsBody.IsGrounded())
             {
@@ -31,11 +39,12 @@ public class Stamina : MonoBehaviour
                 if(currStamina < 0) currStamina = 0;
                 stamina.setStamina(currStamina);
             }
+            
         }
         else if(Input.GetKeyDown(KeyCode.Space) && physicsBody.IsGrounded())
         {
             currStamina -= 5;
-            if(currStamina < 0) currStamina = 0;
+            if(currStamina <= 0) currStamina = 0;
             stamina.setStamina(currStamina);
         }
         else
@@ -44,6 +53,10 @@ public class Stamina : MonoBehaviour
             {
                 currStamina += 2.5f * Time.deltaTime;
                 stamina.setStamina(currStamina);
+                if(currStamina == 0)
+                {
+                    player.SetBool("isWalking", true);
+                }
             }
             
         }
